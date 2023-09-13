6dcf364035d18e2014c691c5a3adefd9c9875486
Staroz <marcourquidi17@gmail.com>
2023-09-13 01:27:52 -0400

    TR-08, moving lists, fixing all issues

--- tests/08-moveList.spec.ts
+++ tests/08-moveList.spec.ts
@@ -28,15 +28,11 @@ test.describe('Moving a list', ()=>{
                 .toContainText(variables.listNameArray[2]);
     });
 
-    // test.afterEach(async ({page})=>{
-    //     // delete board and Workspace
-    //     // const response = await api.deleteWorkspaceApi(variables.workspaceName, variables.boardName);
-    //     // expect(response.status()).toBe(200);
-    //     // logout account 
-    //     await page.goto(url.boards);
-    //     await userPage.logout();
-    //     await expect(page).toHaveURL(url.home);
-    // })   
+    test.afterEach(async ({page})=>{
+        // delete board and Workspace
+        const response = await api.deleteWorkspaceApi(variables.workspaceName, variables.boardName);
+        expect(response.status()).toBe(200);
+    })   
 });
 
 
